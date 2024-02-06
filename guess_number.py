a30a126881bd91483404a448ce37ea56cee6bf64
Riyaad <riyaadhowlader@gmail.com>
2024-02-05 20:38:24 -0600

    Created a tracking system for the number of guesses and put a cap of 5 before it is game over.

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,20 @@
 number = 10
+guesses_made = 0
 
 print('I\'m thinking of a number...')
 guess = input('What number am I thinking of? ')
+guesses_made += 1
 
 while True:
+    if guesses_made == 5:
+        print(f'You are out of guesses! The number was {number}')
+        break
     if guess == str(number):
         print(('Congratulations! You guessed the right number.'))
         break
-    if guess == 'q':
+    elif guess == 'q':
         print(f'The number was {number}.')
         break
     else:
-        guess = input('Incorrect, try again. ')
+        guess = input(f'Incorrect, try again ({5 - guesses_made} tries left). ')
+        guesses_made += 1
