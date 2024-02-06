3976909f6b2294306e22b0154135324732a10b17
Riyaad <riyaadhowlader@gmail.com>
2024-02-05 20:19:30 -0600

    Added a while loop to get continuous user input; added ability to quit program with 'q'

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,14 @@
 number = 10
 
 print('I\'m thinking of a number...')
-guess = int(input('What number am I thinking of? '))
+guess = input('What number am I thinking of? ')
 
-if guess == number:
-    print(('Congratulations! You guessed the right number.'))
-else:
-    print(f'Sorry! The number was {number}.')
+while True:
+    if guess == str(number):
+        print(('Congratulations! You guessed the right number.'))
+        break
+    if guess == 'q':
+        print(f'The number was {number}.')
+        break
+    else:
+        guess = input('Incorrect, try again. ')
